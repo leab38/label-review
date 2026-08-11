6df71fb4b88b18a05fbbcecd6dacc294c7e73acc
leab38 <43719429+leab38@users.noreply.github.com>
2026-08-11 16:07:23 -0500

    Add defaults and update app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-# import read_form_paddleocr
+import read_form_paddleocr
 sample_file = 'label images/sample_label_form.csv'
 
 st.title("Label Review")
@@ -15,3 +15,54 @@ mode = st.radio("Select Mode", ("Use Sample Data","Upload Files"),key="mode")
 
 options = ["wine", "distilled spirits", "malt beverages"]
 product = st.selectbox("Select Product Type", options,key="product")
+
+#default images
+if product == 'distilled spirits':
+    label_front_path = 'label images/Distilled_Spirits_TTB_Ex_Front.png'
+    label_back_path = 'label images/Distilled_Spirits_TTB_Ex_Back.png'
+elif product == 'wine':
+    label_front_path = 'label images/Wine_TTB_Ex_Front.png'
+    label_back_path = 'label images/Wine_TTB_Ex_Back.png'
+
+#file upload
+
+if mode == "Upload Files":
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+        # Read the uploaded CSV file
+        data = pd.read_csv(uploaded_file)
+
+        # Display the data in a table
+        st.write("Uploaded Data:")
+        st.dataframe(data)
+
+    label_front = st.file_uploader("Upload Label Front", type="png")
+    if label_front is not None:
+        st.write("Label Front Uploaded")
+
+    label_back = st.file_uploader("Upload Label Back", type="png")
+    if label_back is not None:
+        st.write("Label Back Uploaded")
+
+if mode == 'Use Sample Data':
+    data = pd.read_csv(sample_file)
+
+# Add a button to start the verification process
+if st.button("Start Verification"):
+    st.write("Verification process started...")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(label_front_path, caption="Label Front")
+    with col2:
+        st.image(label_back_path, caption="Label Back")
+
+    requirements_dict = read_form_paddleocr.main(mode,product,data)
+    st.session_state.verification = True
+
+    st.write("Verification Results:")
+    for key, value in requirements_dict.items():
+        st.write(value[-1])
+
+if st.session_state.verification == True:
+    print(requirements_dict)
